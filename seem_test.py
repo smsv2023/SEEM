ec76e9da336d06fd39242b11b7c7d125565b34ec
smsv2023 <133697387+smsv2023@users.noreply.github.com>
2023-05-27 16:52:10 -0700

    Update seem_test.py

--- seem_test.py
+++ seem_test.py
@@ -64,32 +64,114 @@ def inference(image, task, *args, **kwargs):
 #        else:
 #            return interactive_infer_image(model, audio, image, task, *args, **kwargs)
 
-print("opening test image...")
-test_image_file = '/home/ec2-user/SAM/Segment-Everything-Everywhere-All-At-Once/demo_code/testset/Test010.png'
-test_image=Image.open(test_image_file)
+# process a specified image file
 
-# Convert the image to RGB mode
-test_image = test_image.convert("RGB")
+def process_file(test_image_file):
+   test_image=Image.open(test_image_file)
 
-# Convert the image to a NumPy array
-image_array = np.array(test_image)
+   # Convert the image to RGB mode
+   test_image = test_image.convert("RGB")
 
-# Normalize the color values to the 0-1 range
-image_array = image_array / 255.0
+   # Convert the image to a NumPy array
+   image_array = np.array(test_image)
 
-# Convert the normalized array back to a PIL Image object
-test_image = Image.fromarray((image_array * 255).astype(np.uint8))
+   # Normalize the color values to the 0-1 range
+   image_array = image_array / 255.0
 
-# Create the image dictionary
-image_dict = {'image': test_image, "mask":None}
+   # Convert the normalized array back to a PIL Image object
+   test_image = Image.fromarray((image_array * 255).astype(np.uint8))
 
-image_dict = {'image': test_image, "mask":None}
+   # Create the image dictionary
+   image_dict = {'image': test_image, "mask":None}
 
-image_dict = {'image': test_image, "mask":None}
+   # Now you can feed the image into your model
+   print("start inferencing...")
+   #result_image, pano_seg, pano_seg_info = inference(image_dict, task='Panoptic')
+   pano_seg, pano_seg_info = inference(image_dict, task='Panoptic')
 
-# Now you can feed the image into your model
-print("start inferencing...")
-result_image = inference(image_dict, task='Panoptic')[0]
-print("saving result image...")
-output_file = os.path.splitext(os.path.basename(test_image_file))[0]+"_result.png"
-result_image.save(output_file, format='PNG')
+   # Save image file with color coded pixels and object names
+   #output_image_file = os.path.splitext(test_image_basename)[0]+"_result.png"
+   #result_image.save(output_image_file, format='PNG')
+   return pano_seg, pano_seg_info, image_array
+
+
+# open the image file and process
+#test_image_file = '/home/ec2-user/SAM/Segment-Everything-Everywhere-All-At-Once/demo_code/testset/Test001.png'
+#process_file(test_image_file)
+
+# get input
+import glob
+input_path = '/home/ec2-user/SAM/Segment-Everything-Everywhere-All-At-Once/demo_code/testset/'
+if input_path is not None:
+    image_names = glob.glob(os.path.join(input_path, "*"))
+    num_images = len(image_names)
+else:
+    print("No input path specified.")
+
+# create output folder
+output_path = '/home/ec2-user/SAM/Segment-Everything-Everywhere-All-At-Once/demo_code/output/'
+if output_path is not None:
+    os.makedirs(output_path, exist_ok=True)
+print("Start processing")
+
+if input_path is not None:
+    if output_path is None:
+        print("Warning: No output path specified. Images will be processed but not shown or stored anywhere.")
+    for index, image_name in enumerate(image_names):
+        basename = os.path.basename(image_name)
+        print("  Processing {} ({}/{})".format(image_name, index + 1, num_images))
+        # open the image file and process
+        pano_seg, pano_seg_info, image_array = process_file(image_name)
+
+        print("saving result files...")
+
+        # Save pano_seg tensor
+        output_pth_file = output_path + os.path.splitext(basename)[0]+"_result.pth"
+        torch.save(pano_seg, output_pth_file)
+
+        # Save pano_seg_info list of dictionaries
+        #import pickle
+        #output_pkl_file = output_path + os.path.splitext(basename)[0]+"_result.pkl"
+        #with open(output_pkl_file, 'wb') as f:
+        #   pickle.dump(pano_seg_info, f)
+
+        import json
+        output_json_file = output_path + os.path.splitext(basename)[0]+"_result.json"
+        # Save pano_seg_info list of dictionaries
+        with open(output_json_file, 'w') as f:
+            json.dump(pano_seg_info, f)
+
+        # Load pano_seg_info list of dictionaries
+        #with open('pano_seg_info.json', 'r') as f:
+        #    pano_seg_info = json.load(f)
+
+        # For each object in pano_seg_info, create a mask and crop the original image
+        print ("creating cropped object and mask files...")
+        # scale the color values from the 0-1 range to 0-255 range
+        image_array = image_array * 255
+        for obj in pano_seg_info:
+            # Create a binary mask for this object
+            print("object", obj['id'])
+
+            mask = (pano_seg == obj['id']).cpu().numpy().astype(np.uint8) * 255
+
+            # Resize the mask to match the image_array dimensions
+            from skimage.transform import resize
+            mask_resized = resize(mask, (image_array.shape[0], image_array.shape[1]))
+
+
+            # Crop the original image using the mask
+            cropped = image_array * np.expand_dims(mask_resized, axis=-1)
+
+            # Save the mask and cropped image to files
+            cropped_image_path=os.path.join(output_path, os.path.splitext(basename)[0])
+            os.makedirs(cropped_image_path, exist_ok=True)
+
+            obj_name = str(obj['id']) + "_" + COCO_PANOPTIC_CLASSES[obj['category_id']]
+            print("saving object: %s..."%obj_name)
+            mask_img = Image.fromarray(mask)
+            #mask_img.save(os.path.join(cropped_image_path, f"{obj['id']}_mask.png"))
+            mask_img.save(os.path.join(cropped_image_path, f"{obj_name}_mask.png"))
+            cropped_img = Image.fromarray(cropped.astype(np.uint8))
+            cropped_img.save(os.path.join(cropped_image_path, f"{obj_name}_cropped.png"))
+
