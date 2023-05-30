7033545e48892a2d85aba1a112403676ff2e62e3
smsv2023 <133697387+smsv2023@users.noreply.github.com>
2023-05-30 11:18:09 -0700

    Create shape_detection.py

---
+++ shape_detection.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+
+# Load the image
+image = cv2.imread('image.jpg', cv2.IMREAD_GRAYSCALE)
+
+# Perform Canny edge detection
+edges = cv2.Canny(image, 50, 150, apertureSize=3)
+
+# Perform Hough Line Transform
+lines = cv2.HoughLines(edges, 1, np.pi/180, 200)
+
+# Draw the lines on the original image
+for line in lines:
+    rho, theta = line[0]
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = a * rho
+    y0 = b * rho
+    x1 = int(x0 + 1000 * (-b))
+    y1 = int(y0 + 1000 * (a))
+    x2 = int(x0 - 1000 * (-b))
+    y2 = int(y0 - 1000 * (a))
+    cv2.line(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+# Show the image
+cv2.imshow('Image with lines', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
